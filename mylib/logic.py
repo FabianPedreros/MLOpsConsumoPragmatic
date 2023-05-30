79f93e8d03ab145aa5f3e88b6bd7c747f9c880bc
Fabian Enrique Pedreros Camargo <32172901+FabianPedreros@users.noreply.github.com>
2023-05-30 14:38:12 +0000

    Se anade nueva funcionalidad a cli

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,8 @@ def wiki(name="War Goddess", lenght=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, lenght)
     return my_wiki
+
+def search_wiki(name): 
+    """Buscar en Wikipedia nombres"""
+    results = wikipedia.search(name)
+    return results
